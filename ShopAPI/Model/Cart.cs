53297f1bf2cb32e5ac2526567b012521af0ac7b5
Sobh6n <sobhankhedry7297@gmail.com>
2024-04-03 15:34:38 +0330

    classes added

---
+++ ShopAPI/Model/Cart.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopAPI.Model
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+    }
+}
---
+++ ShopAPI/Model/CartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopAPI.Model
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+
+    }
+}
