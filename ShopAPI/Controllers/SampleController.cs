375b6f46eddaf2a098c85af1570518b66be8df62
Sobh6n <sobhankhedry7297@gmail.com>
2024-04-06 12:05:11 +0330

    added a package and worked

---
+++ ShopAPI/Controllers/SampleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShopAPI.Data;
+
+namespace ShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        private OnlineShopDbContext _dbContext;
+        public SampleController(OnlineShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Fuck()
+        {
+            return Ok("Everything is fuckd");
+        }
+    }
+}
