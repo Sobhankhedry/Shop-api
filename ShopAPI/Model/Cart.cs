3960ed08f8d21313ba32a7ea30e2482d64c048ca
Sobh6n <sobhankhedry7297@gmail.com>
2024-04-06 18:05:12 +0330

    needs to change th whole structure

--- ShopAPI/Model/Cart.cs
+++ ShopAPI/Model/Cart.cs
@@ -3,8 +3,6 @@
     public class Cart
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
-        public int CartItemId { get; set; }
         public ICollection<CartItem>? Items { get; set; }
     }
 }
