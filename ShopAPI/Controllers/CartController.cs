27a34d7597acfc117172f8666431b9edd02449d7
Sobh6n <sobhankhedry7297@gmail.com>
2024-04-07 15:07:20 +0330

    everything workking

---
+++ ShopAPI/Controllers/CartController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+
+    }
+}
